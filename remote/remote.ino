3670fdaadda5556d452bdbe8d4af9a38cbd7e223
Allen Jiang <alljiang@hotmail.com>
2024-03-01 11:01:14 -0600

    fix garage stuff

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,3 +1,4 @@
+#include "sumedh_garage.h"
 #include <ELECHOUSE_CC1101_SRC_DRV.h>
 
 // M1101
@@ -5,28 +6,6 @@
 #define PIN_GDO2 4
 
 #define FREQUENCY (318.00)
-
-// const int signal[] = {
-//     913, -16934, 981, -2034, 947, -5040, 941, -5016, 973, -7988, 951, -5010,
-//     969, -2040,  961, -4994, 963, -5022, 949, -5044, 941, -5040, 939, -5038,
-//     937, -5020,  953, -7998, 941, -5026, 941, -5016, 981, -2046, 943, -5016,
-//     973, -7968,  959, -2060, 939, -5036, 935, -5024, 961, -8000, 939, -2048,
-//     973, -16886, 863, -2276, 717};
-
-const int signal[] = {
-    913, -16934, 981, -2034, 947, -5040, 941, -5016, 973, -7988, 951, -5010,
-    969, -2040,  961, -4994, 963, -5022, 949, -5044, 941, -5040, 939, -5038,
-    937, -5020,  953, -7998, 941, -5026, 941, -5016, 981, -2046, 943, -5016,
-    973, -7968,  959, -2060, 939, -5036, 935, -5024, 961, -8000, 939, -2048,
-    973, -16886, 863, -2276, 717, -5176, 855, -5094, 903, -8034, 913, -5054,
-    937, -2042,  971, -5002, 971, -5020, 953, -5030, 951, -5010, 969, -5020,
-    943, -5016,  951, -7992, 959, -5024, 949, -5006, 973, -2036, 963, -5000,
-    959, -8002,  943, -2058, 945, -5040, 939, -5026, 943, -7990, 973, -2036,
-    963, -16882, 847, -2294, 699, -5214, 849, -5080, 899, -8038, 939, -5014,
-    939, -2066,  967, -5016, 941, -5022, 979, -5012, 943, -5028, 945, -5012,
-    981, -5020,  937, -7982, 983, -5012, 939, -5022, 943, -2068, 941, -5026,
-    943, -7978,  965, -2074, 935, -5024, 955, -5028, 953, -7988, 945, -2066,
-    943, -16874, 871, -2280, 727};
 
 void setup() {
   Serial.begin(9600);
@@ -71,35 +50,45 @@ void initCC1101(float mhz) {
 }
 
 void loop() {
-  sendSamples(signal, sizeof(signal) / sizeof(int), FREQUENCY);
+  sendSamples(FREQUENCY);
 
   delay(1000);
 }
 
-void sendSamples(const int samples[], int samplesLenght, float mhz) {
+void sendSamples(float mhz) {
+  uint32_t sum = 0;
   initCC1101(mhz);
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.print("Transmitting ");
-  Serial.print(samplesLenght);
+  Serial.print(NUM_SAMPLES);
   Serial.println(" Samples");
 
-  int delay = 0;
+  int32_t delay_us = 0;
   unsigned long time;
   byte n = 0;
 
-  for (int i = 0; i < samplesLenght; i++) {
+  for (int i = 0; i < NUM_SAMPLES; i++) {
     // TRANSMIT
     n = 1;
-    delay = samples[i];
-    if (delay < 0) {
+    delay_us = (int32_t)pgm_read_dword_near(signal + i);
+    if (delay_us < 0) {
       // DONT TRANSMIT
-      delay = delay * -1;
+      delay_us = delay_us * -1;
       n = 0;
     }
 
     digitalWrite(PIN_GDO0, n);
+    digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) == 0 ? HIGH : LOW);
 
-    delayMicroseconds(delay);
+    long start = micros();
+    if (delay_us < 10000) {
+        delayMicroseconds(delay_us);
+    }
+    else {
+        delay(delay_us/1000);
+        delayMicroseconds(delay_us%1000);
+    }
+    
   }
 
   // STOP TRANSMITTING
